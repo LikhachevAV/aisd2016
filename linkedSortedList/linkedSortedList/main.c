f26daa9421db9975d0f753a900387d5a54f59c7a
YolloPukki123 <temik_l@mail.ru>
2016-09-26 01:14:46 +0400

    Программа проверяет количество аргументов на входе в программу.

---
+++ linkedSortedList/linkedSortedList/main.c
@@ -0,0 +1,10 @@
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		puts("Argument count error!\nUsage: linkedSortedList.exe <fileName>\n");
+		return 1;
+	}
+	puts("Success!!!");
+	return 0;
+}
