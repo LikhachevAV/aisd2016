b61a684f099e199134367e3784a14ffa9ef4f91c
YolloPukki123 <temik_l@mail.ru>
2016-09-26 15:33:18 +0400

    Добавил проверку на возможность открыть входной файл.

--- linkedSortedList/linkedSortedList/main.c
+++ linkedSortedList/linkedSortedList/main.c
@@ -1,10 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char* argv[])
 {
+	FILE *filePtr;
 	if (argc != 2)
 	{
-		puts("Argument count error!\nUsage: linkedSortedList.exe <fileName>\n");
+		printf("Argument count error!\nUsage: linkedSortedList.exe <fileName>\n");
 		return 1;
 	}
-	puts("Success!!!");
+	printf("fileName:  %s %c", argv[1], '\n');
+	if (!(filePtr = fopen(argv[1], "r")))
+	{
+		printf("Error! File %s %s", argv[1], "can't be open!\n");
+		return 1;
+	}
+
+	printf("Success!!!\n");
 	return 0;
 }
