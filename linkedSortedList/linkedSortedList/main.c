93969de2e87eb23b805cedaa55efdd970eb61ef9
YolloPukki123 <temik_l@mail.ru>
2016-09-27 00:52:54 +0400

    Корректно читает первое число в строку

--- linkedSortedList/linkedSortedList/main.c
+++ linkedSortedList/linkedSortedList/main.c
@@ -4,18 +4,23 @@
 int main(int argc, char* argv[])
 {
 	FILE *filePtr;
+	char s[10];
+	int listSize = 0;
 	if (argc != 2)
 	{
 		printf("Argument count error!\nUsage: linkedSortedList.exe <fileName>\n");
 		return 1;
 	}
-	printf("fileName:  %s %c", argv[1], '\n');
+	
 	if (!(filePtr = fopen(argv[1], "r")))
 	{
-		printf("Error! File %s %s", argv[1], "can't be open!\n");
+		printf("Error! File %s %s", argv[1], "can't be open or not exists!\n");
 		return 1;
 	}
+	fscanf(filePtr, "%s", s);
+	printf("Main list size is: %s %c", s, '\n');
 
+	fclose(filePtr);
 	printf("Success!!!\n");
 	return 0;
 }
