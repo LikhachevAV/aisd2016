528f1cc7416960bfde2ae72b4b968fbc8d247505
Артем Лихачев <artem.lihachev@travelline.ru>
2016-10-31 17:10:26 +0300

    Добавил функции addNode(...), printNode(...)

--- linkedSortedList/main.cpp
+++ linkedSortedList/main.cpp
@@ -4,20 +4,51 @@
 
 using namespace std;
 
+struct Node {
+	double value;
+	Node *next = NULL;
+};
+
+void addNode(Node* node, double value)
+{
+	Node *tmpNode = node;
+	while (node->next != NULL)
+	{
+		node = node->next;
+	}
+	node->next = new Node;
+	node->next->next = NULL;
+	node->value = value;
+	node = tmpNode;
+}
+
+void printNode(Node* node)
+{
+	Node *tmpNode = node;
+	while (node->next != NULL)
+	{
+		cout << " " << node->value;
+		node = node->next;
+	}
+	node = tmpNode;
+}
+
 int main() {
-	double value1, value2;
+
 	cout << "Please, insert  values: ";
 
-	istream_iterator<double> eos;              // end-of-stream iterator
+	istream_iterator<double> eos;        // end-of-stream iterator
 	istream_iterator<double> iit(cin);   // stdin iterator
 
-	cout << "Inserted values: ";
+	Node *head = new Node;
 
 	while (iit != eos)
 	{
-		cout << *iit << " ";
+		addNode(head, *iit);
 		++iit;
 	}
+	cout << "Inserted values: ";
+	printNode(head);
 	cout << endl;
 
 	return 0;
