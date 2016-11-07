fb19b0fa157543afd258e1e5e3787d666e5edd8f
YolloPukki123 <temik_l@mail.ru>
2016-11-07 20:24:24 +0300

    Скопировал проект во временный каталог

---
+++ ReadNumberFromStream/linkedSortedList/VectorProcessor.cpp
@@ -0,0 +1,34 @@
+﻿#include "stdafx.h"
+#include "VectorProcessor.h"
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <boost/range/algorithm/transform.hpp>
+#pragma warning (push, 3)
+#include <boost/phoenix.hpp>
+#pragma warning (pop)
+
+using namespace std;
+using namespace std::placeholders;
+using namespace boost::phoenix::placeholders;
+using namespace boost::phoenix;
+using boost::transform;
+
+double GetVectorsMinVal(std::vector<double> & numbers)
+{
+	return *min_element(numbers.begin(), numbers.end());
+}
+
+void SortVector(std::vector<double> & numbers)
+{
+	sort(numbers.begin(), numbers.end());
+}
+
+void ProcessVector(std::vector<double> & numbers)
+{
+	if (!numbers.empty())
+	{
+		double minVal = GetVectorsMinVal(numbers);
+		boost::transform(numbers, numbers.begin(), arg1 * minVal);
+	}
+}
---
+++ ReadNumberFromStream/linkedSortedList_tests/VectorProcessorTests.cpp
@@ -0,0 +1,38 @@
+﻿#include "stdafx.h"
+#include "..\linkedSortedList\VectorProcessor.h"
+
+using namespace std;
+
+bool VectorsAreEqual(vector<double> const& x, vector<double> const& y)
+{
+	return x == y;
+}
+
+// Функция ProcessVector
+BOOST_AUTO_TEST_SUITE(ProcessVector_function)
+
+	// Создает пустой вектор из пустого вектора
+	BOOST_AUTO_TEST_CASE(dont_change_empty_vector)
+	{
+		vector<double> emptyVector;
+		ProcessVector(emptyVector);
+		BOOST_CHECK(emptyVector.empty());
+	}
+
+BOOST_AUTO_TEST_CASE(squaring_vector_from_just_one_element)
+{
+	vector<double> myVector = { -7.0 };
+	vector<double> expectedVector = { 49.0 };
+	ProcessVector(myVector);
+	BOOST_CHECK(VectorsAreEqual(myVector, expectedVector));
+}
+
+	BOOST_AUTO_TEST_CASE(multiply_every_vectors_element_on_min_element)
+	{
+		vector<double> myVector = { 0.0, 1.0, -5.0 };
+		vector<double> expectedVector = { 0.0, -5.0, 25.0 };
+		ProcessVector(myVector);
+		BOOST_CHECK(VectorsAreEqual(myVector, expectedVector));
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
