59df196202fbd6141ac8f97e5783a2714ade53b7
YolloPukki123 <temik_l@mail.ru>
2016-11-21 00:35:07 +0300

    Добавил проект tree

---
+++ tree/tree/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+const int DL = 200;
+
+struct Tree
+{
+	char name[DL];
+	int urov;
+	Tree *fath, *left, *right;
+};
+
+int ReadFromFile(FILE *f, Tree **r)
+{
+}
+
+int main()
+{
+	return 0;
+}
