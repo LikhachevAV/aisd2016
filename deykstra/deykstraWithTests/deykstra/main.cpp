0c228dcf3e9830d6a2b77c573ed4c470d8c92832
Артем Лихачев <artem.lihachev@travelline.ru>
2017-01-27 17:08:47 +0300

    Добавил вывод меню

--- deykstra/deykstraWithTests/deykstra/main.cpp
+++ deykstra/deykstraWithTests/deykstra/main.cpp
@@ -200,5 +200,40 @@ int main(int argc, char* argv[])
 		}
 	}
 	printNodes();
+
+	auto handleMenuCommand = [&]() {
+		bool exit = false;
+		do
+		{
+			cout << endl;
+			cout << "The following actions are possible: " << endl
+				<< "E - exit program" << endl
+				<< "C - see cities table" << endl
+				<< "D - enter destination city index to see path from source city" << endl << endl;
+			cout << "Please, enter command : ";
+			char answer = 'x';
+			cin >> answer;
+			if (cin.eof() || answer == 'E' || answer == 'e')
+			{
+				cout << "Exit" << endl; //TODO: delete this
+				exit = true;
+			} else
+			if (answer == 'C' || answer == 'c')
+			{
+				cout << "See cities table" << endl; //TODO: delete this
+				//TODO print cities table
+			} else
+			if (answer == 'D' || answer == 'd')
+			{
+				cout << "Print path" << endl; //TODO: delete this
+				//TODO print path
+			}
+			else
+			{
+				cout << "Incorrect command!" << endl;
+			}
+		} while (exit == false);
+	};
+	handleMenuCommand();
 	return EXIT_SUCCESS;
 }
